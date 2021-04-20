be9549dec7ba8f3d32463e74b5dda8726195c0f8
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2021-04-20 18:42:41 -0300

    feat: add tipagens nota de venda

---
+++ src/types/venda.ts
@@ -0,0 +1,52 @@
+export type NotaVendaType = {
+  embarcador: string
+  filial: string
+  numeronota: number
+  serie: string
+  dataemissao: string
+  dataembarque: string
+  previsaoentrega: string
+  cliente_cnpj: string
+  cliente_nome: string
+  transportadora_cnpj: string
+  idmotorista: number
+  transportadora_nome: string
+  cidade: string
+  uf: string
+  idpedidovenda: number
+  inclusaopedido: string
+  idnfesefaz: string
+  tipoentrega: string
+  idromaneio: string
+  placa: string
+  motorista_nome: string
+  tipopedido: string
+  mov_canal: string
+}
+
+export type NotaVendaResponseType = {
+  embarque: {
+    romaneio: string,
+    numero: string,
+    serie: string,
+    chave: string,
+    dtEmissao: string,
+    dtEmbarque: string
+  },
+  embarcador: {
+    nome: string,
+    cnpj: string
+  },
+  destinatario: {
+    nome: string,
+    cnpj: string,
+    endereco: {
+      uf: string,
+      cidade: string
+    }
+  },
+  transportadora: {
+    cnpj: string
+  },
+  tags: string[]
+}

ce2c2129db90700f231ffc20b5af16b933f989ab
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2021-04-22 19:23:06 -0300

    refactor: extrai logica para funcao auxiliar

--- src/controllers/venda.ts
+++ src/controllers/venda.ts
@@ -3,6 +3,7 @@ import { logger } from "../helpers/log";
 import { StatusIntegracao } from "../helpers/status";
 import { getToken, insertOrUpdateStatus, send } from "../services/confirma-facil";
 import { getNotasVenda } from "../services/venda";
+import { NotaVendaResponseType } from "../types/venda";
 
 export const vendaController = async () => {
   try {
@@ -13,18 +14,20 @@ export const vendaController = async () => {
     }
     const notas = await getNotasVenda();
     const token = await getToken(email, senha);
-    logger(notas.length + " Notas de Venda.")
-    for (let i = 0; i < notas.length; i++) {
-      const response = await send(notas[i], token);
-      const { numero, romaneio } = notas[i].embarque;
-      const status = response.statusCode == 200 ? StatusIntegracao.ENVIADA : StatusIntegracao.PENDENTE;
-      const message = await insertOrUpdateStatus(Number(numero), Number(romaneio), status);
-      logger(`${i} - ${response.message} | ${message}`);
-    }
-    logger("Notas de Venda Enviadas.")
+    await exec(notas, token);
   } catch (e) {
     logger(e.message);
   }
 }
 
-
+const exec = async (notas: NotaVendaResponseType[], token: string) => {
+  logger(notas.length + " Notas de Venda.");
+  for (let i = 0; i < notas.length; i++) {
+    const response = await send(notas[i], token);
+    const { numero, romaneio } = notas[i].embarque;
+    const status = response.statusCode == 200 ? StatusIntegracao.ENVIADA : StatusIntegracao.PENDENTE;
+    const message = await insertOrUpdateStatus(Number(numero), Number(romaneio), status);
+    logger(`${i} - ${response.message} | ${message}`);
+  }
+  logger("Notas de Venda Enviadas.");
+}

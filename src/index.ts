ac54c3120f397884623fc9460b498e267c246648
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2021-04-23 16:57:36 -0300

    feat: implementa cron para cada tipo de nota

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import { CronJob } from "cron";
+import { canceladaController } from "./controllers/cancelada";
+import { transferenciaController } from "./controllers/transferencia";
 import { vendaController } from "./controllers/venda";
 
-vendaController();
+const TIME_ZONE = "America/Sao_Paulo";
+
+new CronJob("0 0 2,6-20/3 * * *", vendaController, null, true, TIME_ZONE)
+  .start();
+
+new CronJob("0 0 3,6-20/3 * * *", transferenciaController, null, true, TIME_ZONE)
+  .start();
+
+new CronJob("0 0 4 * * *", canceladaController, null, true, TIME_ZONE)
+  .start();
